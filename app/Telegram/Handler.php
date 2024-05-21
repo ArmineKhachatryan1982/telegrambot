c4f276b3a75dc27d0ed16a447676c74e6e357e08
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-05-21 17:41:09 +0400

    add function hello

--- app/Telegram/Handler.php
+++ app/Telegram/Handler.php
@@ -2,5 +2,9 @@
 namespace App\Telegram;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
 class Handler extends WebhookHandler{
-// 
+    public function hello(){
+
+        $this->reply('hello, it is your first bot in laravel');
+    }
+
 }
