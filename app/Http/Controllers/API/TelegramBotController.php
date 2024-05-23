14b45fef82922144c04275048be0e1b7c44f0e84
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-05-23 16:49:50 +0400

    77

--- app/Http/Controllers/API/TelegramBotController.php
+++ app/Http/Controllers/API/TelegramBotController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Telegram\Bot\Laravel\Facades\Telegram;
 use Telegram\Bot\Api;
+use Telegram\Bot\Laravel\Facades\Telegram;
 class TelegramBotController extends Controller
 {
     protected $telegram;
@@ -16,46 +16,43 @@ class TelegramBotController extends Controller
     }
     public function getMe(){
 
-
-    
         $response = $this->telegram->getMe();
         return $response;
-
     }
-    public function getChatInfo(Request $request){
+    // public function getChatInfo(Request $request){
 
 
-            // Get chat ID from the request
-            $chatId = $request->input('message.chat.id');
+    //         // Get chat ID from the request
+    //         $chatId = $request->input('message.chat.id');
 
-            // Fetch chat information using Telegram Bot SDK
-            $chatInfo = Telegram::getChat(['chat_id' => $chatId]);
+    //         // Fetch chat information using Telegram Bot SDK
+    //         $chatInfo = Telegram::getChat(['chat_id' => $chatId]);
 
-            // Fetch members count using Telegram Bot SDK
-            $membersCount = Telegram::getChatMembersCount(['chat_id' => $chatId]);
+    //         // Fetch members count using Telegram Bot SDK
+    //         $membersCount = Telegram::getChatMembersCount(['chat_id' => $chatId]);
 
-            // Fetch administrators using Telegram Bot SDK
-            $administrators = Telegram::getChatAdministrators(['chat_id' => $chatId]);
+    //         // Fetch administrators using Telegram Bot SDK
+    //         $administrators = Telegram::getChatAdministrators(['chat_id' => $chatId]);
 
-            // Prepare the detailed info message
-            $infoMessage = sprintf(
-                "Chat Info:\nID: %s\nType: %s\nTitle: %s\nUsername: %s\nMembers Count: %d\nAdministrators: %s\n",
-                $chatInfo['id'],
-                $chatInfo['type'],
-                $chatInfo['title'],
-                $chatInfo['username'],
-                $membersCount,
-                implode(', ', array_column($administrators, 'user.username'))
-            );
+    //         // Prepare the detailed info message
+    //         $infoMessage = sprintf(
+    //             "Chat Info:\nID: %s\nType: %s\nTitle: %s\nUsername: %s\nMembers Count: %d\nAdministrators: %s\n",
+    //             $chatInfo['id'],
+    //             $chatInfo['type'],
+    //             $chatInfo['title'],
+    //             $chatInfo['username'],
+    //             $membersCount,
+    //             implode(', ', array_column($administrators, 'user.username'))
+    //         );
 
-            // Respond with the chat information
-            return response()->json(['text' => $infoMessage]);
-        }
+    //         // Respond with the chat information
+    //         return response()->json(['text' => $infoMessage]);
+    //     }
 
 
 
-    public function webhook(){
+    // public function webhook(){
 
-    }
+    // }
 
 }
